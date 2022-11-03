eb233e4a85219ac4519f65407b97bb190fa026bb
calebtripp <calebtripp@outlook.com>
2022-11-03 14:45:08 -0400

    Restructure complete, un needed classes removed, functional app. Needs Styling and non multiple item logic.

--- PackForTheWeather/Controllers/HomeController.cs
+++ PackForTheWeather/Controllers/HomeController.cs
@@ -18,153 +18,150 @@ namespace PackForTheWeather.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var ztp = new ZipTripComfort();
+            return View(ztp);
         }
-        //public IActionResult GetTripInfo(ZipTripComfort input)
-        //{
-        //    Dictionary<string, int> packList = new Dictionary<string, int>()
-        //    {
-        //        { "T Shirt", 0 },
-        //        { "Long Sleeve T", 0 },
-        //        { "Hoodie or Sweater", 0 },
-        //        { "Light Jacket", 0 },
-        //        { "Heavy Coat", 0 },
 
-        //        { "Pants", 0 },
-        //        { "Shorts", 0 },
+        public IActionResult GetTripInfo(ZipTripComfort input)
+        {
+            Dictionary<string, int> packList = new Dictionary<string, int>()
+            {
+                { "T Shirt", 0 },
+                { "Long Sleeve T", 0 },
+                { "Hoodie or Sweater", 0 },
+                { "Light Jacket", 0 },
+                { "Heavy Coat", 0 },
 
-        //        { "Hat and Gloves", 0 },
-        //        { "Scarf", 0 },
-        //        { "Umbrella", 0 },
-        //        { "Sunscreen, Swimsuit, Towel", 0 },
+                { "Pants", 0 },
+                { "Shorts", 0 },
 
-        //        { "Open Toe Shoes", 0 },
-        //        { "Closed Toe Shoes", 0 },
-        //        { "Boots", 0 },
-        //    };
-        //    var client = new HttpClient();
-        //    string APIKey = "269fcd68b6d3d0d501d5f58d3d61fc61";
-        //    var apiCoordinateCall = $"http://api.openweathermap.org/geo/1.0/zip?zip={input.Zip},US&appid={APIKey}";
-        //    var coordinates = client.GetStringAsync(apiCoordinateCall).Result;
-        //    var destination = JObject.Parse(coordinates);
-        //    var lon = destination.GetValue("lon").ToString();
-        //    var lat = destination.GetValue("lat").ToString();
+                { "Hat and Gloves", 0 },
+                { "Scarf", 0 },
+                { "Umbrella", 0 },
+                { "Sunscreen, Swimsuit, Towel", 0 },
 
-        //    var apiWeatherCall = $"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,alerts&appid={APIKey}&units=imperial";
-        //    var FullForecastFromAPI = client.GetStringAsync(apiWeatherCall).Result;
-        //    var dailyForecast = JObject.Parse(FullForecastFromAPI).GetValue("daily").ToString();
+                { "Open Toe Shoes", 0 },
+                { "Closed Toe Shoes", 0 },
+                { "Boots", 0 },
+            };
 
-        //    var dayToGet = input.Trip;
+            var key = System.IO.File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+            var client = new HttpClient();
 
-        //    for (int i = 0; i < input.Trip; i++)
-        //    {
-        //        var dayParse = JArray.Parse(dailyForecast).ElementAt(dayToGet).ToString();
+            var apiCoordinateCall = $"http://api.openweathermap.org/geo/1.0/zip?zip={input.Zip},US&appid={APIKey}";
+            var coordinates = client.GetStringAsync(apiCoordinateCall).Result;
+            var destination = JObject.Parse(coordinates);
+            var lon = destination.GetValue("lon").ToString();
+            var lat = destination.GetValue("lat").ToString();
 
-        //        var dailyTemps = JObject.Parse(dayParse).GetValue("feels_like").ToString();
-        //        var feelsLike = JObject.Parse(dailyTemps).GetValue("day").ToString();
+            var apiWeatherCall = $"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,alerts&appid={APIKey}&units=imperial";
+            var FullForecastFromAPI = client.GetStringAsync(apiWeatherCall).Result;
+            var dailyForecast = JObject.Parse(FullForecastFromAPI).GetValue("daily").ToString();
 
-        //        double comfAdjust = Convert.ToDouble(feelsLike);
-        //        if (input.ComfortLevel == 1)
-        //        {
-        //            comfAdjust -= 5;
-        //        }
-        //        else if (input.ComfortLevel == 2)
-        //        {
-        //            comfAdjust += 5;
-        //        }
+            var dayToGet = input.Trip -= 1; //-=1 allows for element indexing starting at zero and returning proper day and overall qty.
 
-        //        var windSpeed = JObject.Parse(dayParse).GetValue("wind_speed").ToString();
-        //        var rainChance = JObject.Parse(dayParse).GetValue("pop").ToString();
+            for (int i = 0; i <= input.Trip; i++)
+            {
+                var dayParse = JArray.Parse(dailyForecast).ElementAt(dayToGet).ToString();
 
-        //        switch (comfAdjust)
-        //        {
-        //            case >= 76:
-        //                packList["T Shirt"]++;
-        //                packList["Shorts"]++;
-        //                packList["Sunscreen, Swimsuit, Towel"]++;
-        //                packList["Open Toe Shoes"]++;
-        //                switch (Convert.ToDouble(rainChance))
-        //                {
-        //                    case >= .35:
-        //                        packList["Umbrella"]++;
-        //                        break;
-        //                }
-        //                break;
+                var dailyTemps = JObject.Parse(dayParse).GetValue("feels_like").ToString();
+                var feelsLike = JObject.Parse(dailyTemps).GetValue("day").ToString();
 
-        //            case >= 60:
-        //                packList["T Shirt"]++;
-        //                packList["Pants"]++;
-        //                packList["Open Toe Shoes"]++;
-        //                switch (Convert.ToDouble(rainChance))
-        //                {
-        //                    case >= .35:
-        //                        packList["Umbrella"]++;
-        //                        break;
-        //                }
-        //                switch (Convert.ToDouble(windSpeed))
-        //                {
-        //                    case >= 14:
-        //                        packList["Hoodie or Sweater"]++;
-        //                        break;
-        //                }
-        //                break;
+                double comfAdjust = Convert.ToDouble(feelsLike);
+                if (input.ComfortLevel == 1)
+                {
+                    comfAdjust -= 5;
+                }
+                else if (input.ComfortLevel == 2)
+                {
+                    comfAdjust += 5;
+                }
 
-        //            case >= 50:
-        //                packList["Long Sleeve T"]++;
-        //                packList["Pants"]++;
-        //                packList["Hoodie or Sweater"]++;
-        //                packList["Closed Toe Shoes"]++;
-        //                switch (Convert.ToDouble(rainChance))
-        //                {
-        //                    case >= .35:
-        //                        packList["Umbrella"]++;
-        //                        break;
-        //                }
-        //                switch (Convert.ToDouble(windSpeed))
-        //                {
-        //                    case >= 14:
-        //                        packList["Light Jacket"]++;
-        //                        break;
-        //                }
-        //                break;
+                var windSpeed = JObject.Parse(dayParse).GetValue("wind_speed").ToString();
+                var rainChance = JObject.Parse(dayParse).GetValue("pop").ToString();
 
-        //            case <= 50:
-        //                packList["Long Sleeve T"]++;
-        //                packList["Pants"]++;
-        //                packList["Hoodie or Sweater"]++;
-        //                packList["Heavy Coat"]++;
-        //                packList["Boots"]++;
-        //                packList["Hat and Gloves"]++;
-        //                switch (Convert.ToDouble(rainChance))
-        //                {
-        //                    case >= .35:
-        //                        packList["Umbrella"]++;
-        //                        break;
-        //                }
-        //                switch (Convert.ToDouble(windSpeed))
-        //                {
-        //                    case >= 14:
-        //                        packList["Scarf"]++;
-        //                        break;
-        //                }
-        //                break;
-        //        }
-        //    }
+                switch (comfAdjust)
+                {
+                    case >= 76:
+                        packList["T Shirt"]++;
+                        packList["Shorts"]++;
+                        packList["Sunscreen, Swimsuit, Towel"]++;
+                        packList["Open Toe Shoes"]++;
+                        switch (Convert.ToDouble(rainChance))
+                        {
+                            case >= .35:
+                                packList["Umbrella"]++;
+                                break;
+                        }
+                        break;
 
+                    case >= 60:
+                        packList["T Shirt"]++;
+                        packList["Pants"]++;
+                        packList["Open Toe Shoes"]++;
+                        switch (Convert.ToDouble(rainChance))
+                        {
+                            case >= .35:
+                                packList["Umbrella"]++;
+                                break;
+                        }
+                        switch (Convert.ToDouble(windSpeed))
+                        {
+                            case >= 14:
+                                packList["Hoodie or Sweater"]++;
+                                break;
+                        }
+                        break;
 
-        //    var vm = new GetTripInfoViewModel();
+                    case >= 50:
+                        packList["Long Sleeve T"]++;
+                        packList["Pants"]++;
+                        packList["Hoodie or Sweater"]++;
+                        packList["Closed Toe Shoes"]++;
+                        switch (Convert.ToDouble(rainChance))
+                        {
+                            case >= .35:
+                                packList["Umbrella"]++;
+                                break;
+                        }
+                        switch (Convert.ToDouble(windSpeed))
+                        {
+                            case >= 14:
+                                packList["Light Jacket"]++;
+                                break;
+                        }
+                        break;
 
-        //    vm.PackList = packList;
-        //    return View(vm);
-        //}
-    
+                    case <= 50:
+                        packList["Long Sleeve T"]++;
+                        packList["Pants"]++;
+                        packList["Hoodie or Sweater"]++;
+                        packList["Heavy Coat"]++;
+                        packList["Boots"]++;
+                        packList["Hat and Gloves"]++;
+                        switch (Convert.ToDouble(rainChance))
+                        {
+                            case >= .35:
+                                packList["Umbrella"]++;
+                                break;
+                        }
+                        switch (Convert.ToDouble(windSpeed))
+                        {
+                            case >= 14:
+                                packList["Scarf"]++;
+                                break;
+                        }
+                        break;
+                }
+            }
 
+            var vm = new GetTripInfoViewModel();
+            vm.PackList = packList;
+            return View(vm);
+        }
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- PackForTheWeather/Models/Day.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PackForTheWeather.Models
-{
-    public class Day
-    {       
-        public double FeelsLike { get; set; }
-        public double WindSpeed { get; set; }
-        public double RainChance { get; set; }
-    }
-}
